320722ff24f6b5e574bb0af1ee881610c9b07e64
Matteo Piccini <gnappo.1@live.it>
2024-04-04 10:44:28 -0700

    lecture starter code

---
+++ 05_client_server_communication/client/src/App.js
@@ -0,0 +1,49 @@
+// 📚 Review With Students:
+    // Request response cycle
+    //Note: This was build using v5 of react-router-dom
+import { Outlet, useNavigate } from 'react-router-dom'
+import {createGlobalStyle} from 'styled-components'
+import {useEffect, useState} from 'react'
+import Header from './components/navigation/Header'
+
+function App() {
+  const [productions, setProductions] = useState([])
+  const [production_edit, setProductionEdit] = useState(false)
+  const navigate = useNavigate()
+
+  //5.✅ GET Productions
+  
+  // 6.✅ navigate to client/src/components/ProductionForm.js
+
+  const addProduction = (production) => setProductions(productions => [...productions,production])
+  const updateProduction = (updated_production) => setProductions(productions => productions.map(production =>{
+    if(production.id === updated_production.id){
+      return updated_production
+    } else {
+      return production
+    }
+  } ))
+  const deleteProduction = (deleted_production) => setProductions(productions => productions.filter((production) => production.id !== deleted_production.id) )
+
+  const handleEdit = (production) => {
+    setProductionEdit(production)
+    navigate(`/productions/${production.id}/edit`)
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <Header handleEdit={handleEdit}/>
+      <Outlet context={{ addProduction, updateProduction, deleteProduction, productions, production_edit }} />
+    </>
+  )
+}
+
+export default App
+
+const GlobalStyle = createGlobalStyle`
+    body{
+      background-color: black; 
+      color:white;
+    }
+    `
---
+++ 05_client_server_communication/client/src/index.js
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import { router } from "./routes";
+
+import "./index.css"
+import { RouterProvider } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+console.log("🚀 ~ router:", router)
+root.render(
+  // <StrictMode>
+  <RouterProvider router={router} />
+  // </StrictMode>
+);
